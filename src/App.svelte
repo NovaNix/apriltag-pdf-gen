a5c5e298d0082c5eb7c138407d51e533ef5ee1e6
Michael Nix <michael.g.nix@gmail.com>
2022-10-04 16:55:30 -0500

    removed unused code

--- src/App.svelte
+++ src/App.svelte
@@ -2,11 +2,6 @@
 import PrintPreview from './lib/preview/PrintPreview.svelte';
 import ConfigPanel from './lib/ConfigPanel.svelte';
 
-function generateTags(config)
-{
-
-}
-
 </script>
 
 <PrintPreview/>
