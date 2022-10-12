eb2c806ce3cbe73e30a124b7ebff3a9a0c366bec
Michael Nix <michael.g.nix@gmail.com>
2022-10-12 12:24:22 -0500

    Turned into a details block

--- src/lib/PrintInfoPanel.svelte
+++ src/lib/PrintInfoPanel.svelte
@@ -2,26 +2,42 @@
 	import {config, previewInfo} from './stores.js';
 </script>
 
-<section id="print-info">
-	<h3>Print Info</h3>
+<details id="print-info">
+	<summary>Print Info</summary>
 	<p>{$previewInfo.pages} pages, {$config.tagCount} tags</p>
 	<p>Tags Per Page: {$previewInfo.tagsPerPage} </p>
 	<p>x: {$previewInfo.tagsPerX}, y: {$previewInfo.tagsPerY}</p>
 	<p id="preview-scale">Preview Scale: {Number($previewInfo.scale).toLocaleString(undefined,{style: 'percent', minimumFractionDigits:2})}</p>
-</section>
+</details>
 
 <style>
 	#print-info {
 		font-size: 1em;
-		background-color: lightgray;
 		padding: 0.5em 1em;
-	}
-
-	h3 {
-		text-align: center;
 	}
 
 	p {
 		text-align: center;
+	}
+
+	details {
+	    border: 1px solid #aaa;
+	    border-radius: 4px;
+	    padding: .5em .5em 0;
+	}
+
+	summary {
+	    font-weight: bold;
+	    margin: -.5em -.5em 0;
+	    padding: .5em;
+	}
+
+	details[open] {
+	    padding: .5em;
+	}
+
+	details[open] summary {
+	    border-bottom: 1px solid #aaa;
+    	margin-bottom: .5em;
 	}
 </style>
